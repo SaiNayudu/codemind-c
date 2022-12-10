8aa9ec857f13bd552ef083516fc8cee8791277a4
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2022-12-10 11:42:29 +0530

    #include<stdio.h> int main() { 	int size,i,sum=0; 	scanf("%d",&size); 	int a[size]; 	//reading of array 	for(i=0;i<size;i++) 	{ 		scanf("%d",&a[i]); 	} 	//printing of array 	for(i=0;i<size;i++) 	{ 			if(a[i]%2!=0) 	{ 		sum=sum+a[i]; 	} 	} 	printf("%d",sum);
    }

---
+++ ODD_Sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int size,i,sum=0;
+	scanf("%d",&size);
+	int a[size];
+	//reading of array
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	//printing of array
+	for(i=0;i<size;i++)
+	{
+			if(a[i]%2!=0)
+	{
+		sum=sum+a[i];
+	}
+	}
+	printf("%d",sum);
+
+}

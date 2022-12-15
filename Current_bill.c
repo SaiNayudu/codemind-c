cb14e07f5c7fb419a1379d545ceced062cb71047
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2022-12-15 11:13:00 +0530

    #include<stdio.h> int main() {     int units;     float bill,ta,tb;     scanf("%d",&units);     if(units<=199)     {         bill=units*1.20;         printf("%.2f",bill+100);     }     else if(units>=200 && units <400)     {         bill = units*1.50;         printf("%.2f",bill+100);     }     else if(units>=400 && units<600)     {         bill = units*1.80;         ta=bill*0.15;         tb=bill+ta;         printf("%.2f",tb);     }     else     {          bill = units*2.00;         ta=bill*0.15;         tb=bill+ta;         printf("%.2f",tb);
        }
    }

---
+++ Current_bill.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+int main()
+{
+    int units;
+    float bill,ta,tb;
+    scanf("%d",&units);
+    if(units<=199)
+    {
+        bill=units*1.20;
+        printf("%.2f",bill+100);
+    }
+    else if(units>=200 && units <400)
+    {
+        bill = units*1.50;
+        printf("%.2f",bill+100);
+    }
+    else if(units>=400 && units<600)
+    {
+        bill = units*1.80;
+        ta=bill*0.15;
+        tb=bill+ta;
+        printf("%.2f",tb);
+    }
+    else 
+    {
+         bill = units*2.00;
+        ta=bill*0.15;
+        tb=bill+ta;
+        printf("%.2f",tb);
+        
+    }
+}

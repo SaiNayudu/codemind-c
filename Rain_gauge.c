dff51f3aa4f4b4eea362e24900a6f5fb7c04a6bf
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2022-12-29 18:31:40 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         int x;         scanf("%d",&x);         if(x<3)         printf("LIGHT ");         else if(x>=7)         printf("HEAVY ");         else         printf("MODERATE ");     } }

---
+++ Rain_gauge.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x<3)
+        printf("LIGHT
+");
+        else if(x>=7)
+        printf("HEAVY
+");
+        else
+        printf("MODERATE
+");
+    }
+}

f82e8b559d067a582ac23219d76e5ec300e117fc
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2023-05-18 21:51:46 +0530

    #include <stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int min_index;     for(int i = 0; i < n - 1; i++) {         min_index = i;         for(int j = i + 1; j < n; j++) {             if(a[min_index] > a[j]) {                 min_index = j;             }         }         if(min_index != i)         {             int temp = a[i];             a[i] = a[min_index];             a[min_index] = temp;         }     }
        for(int i = 0; i < n; i++)  {
            printf("%d ", a[i]);
        }
        return 0;
    }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int min_index;
+    for(int i = 0; i < n - 1; i++) {
+        min_index = i;
+        for(int j = i + 1; j < n; j++) {
+            if(a[min_index] > a[j]) {
+                min_index = j;
+            }
+        }
+        if(min_index != i)
+        {
+            int temp = a[i];
+            a[i] = a[min_index];
+            a[min_index] = temp;
+        }
+    }
+    
+    for(int i = 0; i < n; i++)  {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}

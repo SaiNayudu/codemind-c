0cab965857ca0e00134a493ea662c4580231d692
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2023-06-14 21:55:10 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++)     {         scanf("%d",&x[i]);         if(x[i]%2!=0)         s=s+x[i];
        }
        printf("%d",s);
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+        if(x[i]%2!=0)
+        s=s+x[i];
+        
+    }
+    printf("%d",s);
+}

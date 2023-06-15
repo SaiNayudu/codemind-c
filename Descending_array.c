111ea4b18c1d6a8e1d250eef51813e35627d8352
SaiNayudu <115731037+SaiNayudu@users.noreply.github.com>
2023-06-15 21:35:43 +0530

    #include<stdio.h> int main() {     int n,cnt=0;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(int i=0;i<n-1;i++)     {         if(x[i]>x[i+1])         cnt++;     }     (cnt==n-1)?printf("yes"):printf("no"); }

---
+++ Descending_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,cnt=0;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        if(x[i]>x[i+1])
+        cnt++;
+    }
+    (cnt==n-1)?printf("yes"):printf("no");
+}
